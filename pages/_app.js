e7fe231febc0ab24744d0099b74fdf6ce479e4bc
Vitor <vitor.andreylopess@gmail.com>
2023-04-08 12:39:06 -0300

    adicionei o vercel analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,13 @@
 import "@/styles/globals.css";
 
 import MainContainer from "@/components/MainContainer";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function App({ Component, pageProps }) {
   return (
     <MainContainer>
       <Component {...pageProps} />
+      <Analytics />
     </MainContainer>
   );
 }
